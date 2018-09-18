1ebe43c7c1e0d0a39f14faad6223331417c352a9
Paulo Köch <paulo.koch@gmail.com>
2018-09-18 22:45:05 +0100

    DNS

---
+++ dns.sh
@@ -0,0 +1,3 @@
+now dns add pko.ch bolinhas A 198.98.53.214
+now dns add pko.ch bolinhas AAAA 2605:6400:1:fed5:22:193b:2fdc:b3c3
+now dns add pko.ch '@' TXT 'keybase-site-verification=RwmNVmbD0xzmAzJCUoRw-gusgJYDWsLo-y0jZRyX1NI'
